9570d3c7a0cedcb20d1611190fd676d5c11a133b
hayyantahirr <hayyantahir7@gmail.com>
2024-09-04 18:52:26 +0500

    style has been created

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,31 +1,11 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
+import { Text } from 'react-native';
+import style from "../../styles/home-css"
 
-
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
-  return (
-   <Text>Hello app</Text>
+  return (<>
+   <Text style={style.title}>The Capture </Text>
+   <Text style={style.subtitle}>Created by React native </Text></>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
---
+++ styles/home-css.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 50,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 10,
+  },
+});
+export default styles;
