7ec1d533f79a016132a966877d18806e87527e17
hayyantahirr <hayyantahir7@gmail.com>
2024-09-07 01:40:06 +0500

    camera added to the project

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,16 +1,46 @@
-import { Text, View } from "react-native";
 import style from "../../styles/home-css";
+import { Button, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { CameraView, useCameraPermissions } from "expo-camera";
 
 export default function HomeScreen() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={style.container}>
+        <Text style={style.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <>
-      <View style={style.titlecontainer}>
-        <Text style={style.title}>The Capture </Text>
-        <Text style={style.subtitle}>Created by React native </Text>
+      <View style={style.titlecontainer} >
+        <Text style={style.title}>The Capture</Text>
+        <Text style={style.subtitle}>Created by React Native</Text>
       </View>
-      <View>
-        
+      <View style={style.cameraContainer}>
+        <CameraView style={style.camera} facing={facing}>
+          <View style={style.buttonContainer}>
+            <TouchableOpacity style={style.button} onPress={toggleCameraFacing}>
+              <Text style={style.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
       </View>
+      <Button title="Capture Your Moment!" />
     </>
   );
 }
--- styles/home-css.jsx
+++ styles/home-css.jsx
@@ -5,18 +5,45 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
     textAlign: "center",
-    
   },
   subtitle: {
     fontSize: 16,
     color: "#fff",
     textAlign: "center",
-    
+    marginBottom: 20, // Added spacing below subtitle
   },
   titlecontainer: {
-    flex: 1,
-    justifyContent: "center",
+    marginTop: 100,
     alignItems: "center",
+  },
+
+  cameraContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 300, // Adjust the height as needed
+    marginBottom: 20, // Spacing below the camera
+  },
+  camera: {
+    width: "100%",
+    height: "100%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 20,
+  },
+  button: {
+    alignSelf: "center",
+    backgroundColor: "#2196F3",
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
   },
 });
 export default styles;
